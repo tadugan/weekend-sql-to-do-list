903a78222447bab21929539a50401dc10081972f
Tim Dugan <tadugan@me.com>
2021-07-03 17:03:52 -0500

    Coded PUT route to update completion status of task. Tested in Postman.

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -7,7 +7,7 @@ const tasksRouter = express.Router();
 // Query to get contents of entire to_do_list table
 tasksRouter.get('/', (req, res) => {
     // variable to hold query text
-    let queryText = 'SELECT * FROM to_do_list;';
+    const queryText = 'SELECT * FROM to_do_list;';
     // SQL query to DB
     pool.query(queryText)
         .then(result => {
@@ -38,7 +38,25 @@ tasksRouter.post('/', (req, res) => {
 });
 
 // PUT
+// PUT request to switch 'completed' value to true
+tasksRouter.put('/:id', (req, res) => {
+    const taskId = req.params.id;
+    const queryText = `
+    UPDATE to_do_list 
+    SET "complete"='TRUE' 
+    WHERE id=$1;
+    `;
 
+    pool.query(queryText, [taskId])
+    .then(response => {
+        console.log('updated this many rows:', response.rowCount); // should be 1
+        res.sendStatus(202);
+    })
+    .catch(error => {
+        console.log('Error in updating status of task. Error:', error);
+        res.sendStatus(500);
+    });
+});
 
 // DELETE
 
