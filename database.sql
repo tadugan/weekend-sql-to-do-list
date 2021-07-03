847c07f4ae2277160882a55eda8548de304328e7
Tim Dugan <tadugan@me.com>
2021-07-03 16:02:27 -0500

    Created database to store tasks. Added table and sample tasks.

--- database.sql
+++ database.sql
@@ -1,1 +1,22 @@
 -- Queries to create and populate the todo task database
+
+-- create table to store to do list
+CREATE TABLE to_do_list (
+	"id" serial PRIMARY KEY,
+	"name" varchar(80),
+	"description" varchar(255),
+	"complete" boolean default 'false'
+);
+
+-- display entire table
+SELECT *
+FROM to_do_list;
+
+-- delete entire to_do_list table
+DROP TABLE to_do_list;
+
+-- populate to_do_list table with sample data
+INSERT INTO to_do_list ("name", "description")
+	VALUES ('Wash Dishes', 'Use soap and water'),
+	('Mow the Lawn', 'Use the lawnmower'),
+	('Cook Dinner', 'Menu tonight is pork chops and applesauce');
